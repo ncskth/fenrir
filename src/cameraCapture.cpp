2a30ad91184e25816866d18b4ac46e7f918f6871
Eben Kadile <eben.kadile24@gmail.com>
2026-08-19 19:47:28 +0200

    tried manual accumulation

--- src/cameraCapture.cpp
+++ src/cameraCapture.cpp
@@ -7,6 +7,32 @@ namespace SlamDemo {
 
     barrier sync_point(2);
 
+    void updateAccumulator(
+        const cv::Size resolution,
+        const double positiveFactor,
+        const double negativeFactor,
+        const dv::EventStore& events,
+        vector<double>& accumulator
+    ) {
+        for(auto ev : events) {
+            if(ev.polarity())  {
+                accumulator[resolution.width*ev.y() + ev.x()] = min(1.0, positiveFactor*accumulator[resolution.width*ev.y() + ev.x()]);
+            }
+            else {
+                accumulator[resolution.width*ev.y() + ev.x()] = max(0.0, negativeFactor*accumulator[resolution.width*ev.y() + ev.x()]);
+            }
+        }
+        //cout << "Accumulator updated!" << endl;
+    }
+
+    cv::Mat generateImage(const cv::Size resolution, vector<double>& accumulator) {
+        cv::Mat result(resolution, CV_8UC1);
+        for(int i = 0; i < resolution.area(); i++) {
+            result.at<uint8_t>(i/resolution.width, i%resolution.width) = (uint8_t)(255*accumulator[i]);
+        }
+        return result;
+    }
+
     void rightCameraCapture(
         const cv::Size resolution,
         const string serial,
@@ -15,6 +41,8 @@ namespace SlamDemo {
         const int highPassMicroseconds,
         const int accumulatorTimeConstant,
         const double accumulatorGain,
+        const double positiveFactor,
+        const double negativeFactor,
         const int sendIntervalMilliseconds,
         const cv::Matx33f cameraMatrix,
         const vector<float> distortionCoeffs,
@@ -31,7 +59,7 @@ namespace SlamDemo {
         }
 
         dv::noise::BackgroundActivityNoiseFilter highPass(resolution, highPassMicroseconds*1us);
-        //dv::noise::LowPassFilter lowPass(resolution, lowPassHz);
+        dv::noise::LowPassFilter lowPass(resolution, 1000.0);
 
         auto hotPixelsX = cnpy::npy_load(hotPixelXFile);
         auto hotPixelsY = cnpy::npy_load(hotPixelYFile);
@@ -46,17 +74,23 @@ namespace SlamDemo {
         //dv::EventStore eventBuffer;
 
         // Initialize an accumulator with some resolution
-        dv::Accumulator accumulator(resolution);
+        //dv::Accumulator accumulator(resolution);
 
         // Apply configuration, these values can be modified to taste
-        accumulator.setMinPotential(0.f);
-        accumulator.setMaxPotential(1.f);
-        accumulator.setNeutralPotential(0.5f);
-        accumulator.setEventContribution(accumulatorGain);
-        accumulator.setDecayFunction(dv::Accumulator::Decay::EXPONENTIAL);
-        accumulator.setDecayParam(1e3*accumulatorTimeConstant);
-        accumulator.setIgnorePolarity(false);
-        accumulator.setSynchronousDecay(false);
+        //accumulator.setMinPotential(0.f);
+        //accumulator.setMaxPotential(1.f);
+        //accumulator.setNeutralPotential(0.5f);
+        //accumulator.setEventContribution(accumulatorGain);
+        //accumulator.setDecayFunction(dv::Accumulator::Decay::EXPONENTIAL);
+        //accumulator.setDecayParam(1e3*accumulatorTimeConstant);
+        //accumulator.setIgnorePolarity(false);
+        //accumulator.setSynchronousDecay(false);
+
+        vector<double> accumulator;
+        accumulator.reserve(resolution.area());
+        for(int i = 0; i < resolution.area(); i++) {
+            accumulator[i] = 0.5;
+        }
 
         cout << "Right camera ready!" << endl;
         sync_point.arrive_and_wait();
@@ -65,27 +99,27 @@ namespace SlamDemo {
 
         while (camera->isRunning()) {
             if (const auto raw = camera->getNextEventBatch()) {
-                //highPass.accept(*raw);
-                //const auto high = highPass.generateEvents();
-                //lowPass.accept(high);
-                //const auto low = lowPass.generateEvents();
-                maskFilter.accept(*raw);
+                highPass.accept(*raw);
+                const auto high = highPass.generateEvents();
+                lowPass.accept(high);
+                const auto low = lowPass.generateEvents();
+                maskFilter.accept(low);
                 const auto masked = maskFilter.generateEvents();
-                //eventBuffer.add(masked);
-                accumulator.accept(masked);
+                updateAccumulator(resolution, positiveFactor, negativeFactor, ref(masked), ref(accumulator));
             }
 
             auto now = chrono::high_resolution_clock::now();
             if (now - lastQPush > sendIntervalMilliseconds * 1ms) {
-                dv::Frame frame = accumulator.generateFrame();
-                cv::Mat imageDistorted = frame.image;
+                //dv::Frame frame = accumulator.generateFrame();
+                //cv::Mat imageDistorted = frame.image;
+                cv::Mat imageDistorted = generateImage(resolution, accumulator);
                 cv::Mat image;
                 cv::undistort(imageDistorted, image, cameraMatrix, distortionCoeffs);
-                cv::Mat blurred;
-                cv::blur(image, blurred, cv::Size(3, 3));
-                outgoingImages1.push(blurred);
+                //cv::Mat blurred;
+                //cv::blur(image, blurred, cv::Size(3, 3));
+                outgoingImages1.push(image);
                 cv::Mat frameToDepth;
-                blurred.convertTo(frameToDepth, CV_64F);
+                image.convertTo(frameToDepth, CV_64F);
                 outgoingImages2.push(frameToDepth);
 
                 sync_point.arrive_and_wait();
@@ -102,6 +136,8 @@ namespace SlamDemo {
         const int highPassMicroseconds,
         const int accumulatorTimeConstant,
         const double accumulatorGain,
+        const double positiveFactor,
+        const double negativeFactor,
         const int sendIntervalMilliseconds,
         const cv::Matx33f cameraMatrix,
         const vector<float> distortionCoeffs,
@@ -123,7 +159,7 @@ namespace SlamDemo {
         }
 
         dv::noise::BackgroundActivityNoiseFilter highPass(resolution, highPassMicroseconds*1us);
-        //dv::noise::LowPassFilter lowPass(resolution, lowPassHz);
+        dv::noise::LowPassFilter lowPass(resolution, 1000.0);
 
         auto hotPixelsX = cnpy::npy_load(hotPixelXFile);
         auto hotPixelsY = cnpy::npy_load(hotPixelYFile);
@@ -139,17 +175,23 @@ namespace SlamDemo {
         vector<dv::IMU> imuBuffer;
 
         // Initialize an accumulator with some resolution
-        dv::Accumulator accumulator(resolution);
+        //dv::Accumulator accumulator(resolution);
 
         // Apply configuration, these values can be modified to taste
-        accumulator.setMinPotential(0.f);
-        accumulator.setMaxPotential(1.f);
-        accumulator.setNeutralPotential(0.5f);
-        accumulator.setEventContribution(accumulatorGain);
-        accumulator.setDecayFunction(dv::Accumulator::Decay::EXPONENTIAL);
-        accumulator.setDecayParam(1e3*accumulatorTimeConstant);
-        accumulator.setIgnorePolarity(false);
-        accumulator.setSynchronousDecay(false);
+        //accumulator.setMinPotential(0.f);
+        //accumulator.setMaxPotential(1.f);
+        //accumulator.setNeutralPotential(0.5f);
+        //accumulator.setEventContribution(accumulatorGain);
+        //accumulator.setDecayFunction(dv::Accumulator::Decay::EXPONENTIAL);
+        //accumulator.setDecayParam(1e3*accumulatorTimeConstant);
+        //accumulator.setIgnorePolarity(false);
+        //accumulator.setSynchronousDecay(false);
+
+        vector<double> accumulator;
+        accumulator.reserve(resolution.area());
+        for(int i = 0; i < resolution.area(); i++) {
+            accumulator[i] = 0.5;
+        }
 
         cout << "Left camera ready!" << endl;
         sync_point.arrive_and_wait();
@@ -158,14 +200,14 @@ namespace SlamDemo {
 
         while (camera->isRunning()) {
             if (const auto raw = camera->getNextEventBatch()) {
-                //highPass.accept(*raw);
-                //const auto high = highPass.generateEvents();
-                //lowPass.accept(high);
-                //const auto low = lowPass.generateEvents();
-                maskFilter.accept(*raw);
+                highPass.accept(*raw);
+                const auto high = highPass.generateEvents();
+                lowPass.accept(high);
+                const auto low = lowPass.generateEvents();
+                maskFilter.accept(low);
                 const auto masked = maskFilter.generateEvents();
-                eventBuffer.add(masked);
-                accumulator.accept(masked);
+                updateAccumulator(resolution, positiveFactor, negativeFactor, ref(masked), ref(accumulator));
+                eventBuffer.add(*raw);
             }
 
             if (const auto imuBatch = camera->getNextImuBatch()) {
@@ -177,15 +219,16 @@ namespace SlamDemo {
                 outgoingEvents.push(eventBuffer);
                 eventBuffer = dv::EventStore();
 
-                dv::Frame frame = accumulator.generateFrame();
-                cv::Mat imageDistorted = frame.image;
+                //dv::Frame frame = accumulator.generateFrame();
+                //cv::Mat imageDistorted = frame.image;
+                cv::Mat imageDistorted = generateImage(resolution, accumulator);
                 cv::Mat image;
                 cv::undistort(imageDistorted, image, cameraMatrix, distortionCoeffs);
-                cv::Mat blurred;
-                cv::blur(image, blurred, cv::Size(3, 3));
-                outgoingImages1.push(blurred);
+                //cv::Mat blurred;
+                //cv::blur(image, blurred, cv::Size(3, 3));
+                outgoingImages1.push(image);
                 cv::Mat frameToDepth;
-                blurred.convertTo(frameToDepth, CV_64F);
+                image.convertTo(frameToDepth, CV_64F);
                 outgoingImages2.push(frameToDepth);
                 outgoingIMU.push(imuBuffer);
                 imuBuffer.clear();
